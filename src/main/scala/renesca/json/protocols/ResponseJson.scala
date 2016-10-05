080e06baa6d87a57f58cb8362e8374dfa57cf78a
Philip Andrew <phil@orsa-studio.com>
2016-10-05 13:07:47 +0800

    Cleaned up but not sure if I deleted something which will make it break, tested with only minor testing

--- src/main/scala/renesca/json/protocols/ResponseJson.scala
+++ src/main/scala/renesca/json/protocols/ResponseJson.scala
@@ -15,39 +15,10 @@ object ResponseJson {
 
   // Need ... Relationship Node Graph Error Data Result Transaction Response
 
-  implicit val formats = Serialization.formats(NoTypeHints) + new PropertyKeySerializer() + new ParameterValueSerializer() + new PropertyValueSerializer()
+  implicit val formats = Serialization.formats(NoTypeHints) + new PropertyKeySerializer() + new PropertyValueSerializer()
 
   def jsonToObject(jsonResponse: String): Response = {
     val p = parse(jsonResponse)
     p.extract[Response]
   }
-
-  def jsonOf(obj: Relationship): String = {
-    write(obj)
-  }
-
-  def jsonOf(obj: Node): String = {
-    write(obj)
-  }
-
-  def jsonOf(obj: Graph): String = {
-    write(obj)
-  }
-
-  def jsonOf(obj: Data): String = {
-    write(obj)
-  }
-
-  def jsonOf(obj: Result): String = {
-    write(obj)
-  }
-
-  def jsonOf(obj: Transaction): String = {
-    write(obj)
-  }
-
-  def jsonOf(obj: Response): String = {
-    write(obj)
-  }
-
 }
--- src/main/scala/renesca/json/protocols/Serializations.scala
+++ src/main/scala/renesca/json/protocols/Serializations.scala
@@ -20,54 +20,6 @@ class PropertyKeySerializer() extends CustomKeySerializer[PropertyKey](format =>
   }
 }))
 
-object ParameterValueSerializer {
-  def parameterValueMatch(p: ParameterValue): JValue = {
-    p match {
-      case LongArrayPropertyValue(that) => {
-        println("@#@# why? 1")
-        null
-      }
-      case DoubleArrayPropertyValue(that) => {
-        println("@#@# why? 1")
-        null
-      }
-      case StringArrayPropertyValue(that) => {
-        println("@#@# why? 1")
-        null
-      }
-      case BooleanArrayPropertyValue(that) => {
-        println("@#@# why? 1")
-        null
-      }
-      case MapParameterValue(keyValuePairs) => {
-        println("Map parameter value ####")
-        new JObject(keyValuePairs.map { case (key, value) => (key.name, {
-          value match {
-            case LongPropertyValue(v) => new JLong(v)
-            case StringPropertyValue(v) => new JString(v)
-            case DoublePropertyValue(v) => new JDouble(v)
-            case BooleanPropertyValue(v) => new JBool(v)
-            case NullPropertyValue => JNull
-            case that: ArrayParameterValue => {
-              println("@#@# why?")
-              null
-            }
-            case _ => {
-              println("@#@# UNSURE !!!!!")
-              null
-            }
-          }
-        })
-        }.toList)
-      }
-      case _ => {
-        println("@#@# why")
-        null
-      }
-    }
-  }
-}
-
 class PropertyValueSerializer() extends CustomSerializer[PropertyValue](format => ( {
   case JString(s) => { StringPropertyValue(s) }
   case JDouble(s) => { DoublePropertyValue(s) }
@@ -77,128 +29,34 @@ class PropertyValueSerializer() extends CustomSerializer[PropertyValue](format =
   case JInt(s) => { LongPropertyValue(s.toLong) }
 
   case JBool(s)   => { BooleanPropertyValue(s) }
-  case JObject(s) => { println("@#@# !!## JObject")
-    null; }
-  case JArray(s) => { println("@#@# !!## JArray")
-    null }
-  case it => {
-    println("@#@# property value serializer 1###")
-    println(it.toString)
-    println("" + it.getClass.toString)
-    null }
 }, {
-
-
-  case LongPropertyValue(v) => JLong(v)
-  case StringPropertyValue(v) => JString(v)
-  case DoublePropertyValue(v) => JDouble(v)
-  case BooleanPropertyValue(v) => JBool(v)
-  case NullPropertyValue => JNull
-  case that: ArrayParameterValue => {
-    println("@#@# why?")
-    null
-  }
-  case it => {
-    println(it.toString)
-    println(it.getClass.toString)
-    println("@#@# UNSURE !!!!!")
-    null
-  }
-
+  case _ => null
 })) {
-
 }
-
-class ParameterValueSerializer() extends CustomSerializer[ParameterValue](format => ( {
-  case JString(s) => { StringPropertyValue(s) }
-  case JDouble(s) => { DoublePropertyValue(s) }
-  case JDecimal(s) => { LongPropertyValue(s.toLong) }
-  case JLong(s) => { LongPropertyValue(s.toLong) }
-  case JInt(s) => { LongPropertyValue(s.toLong) }
-  case JBool(s)   => { BooleanPropertyValue(s) }
-  case JObject(s) => { println("!!## JObject")
-    null; }
-  case JArray(s) => { println("!!## JArray")
-    null }
-  case _ => { println("!!## Unknown"); null }
-}, {
-  case Request(s) => { null }
-}))
 
 object ParameterMapSerializer {
   def parameterValueOf(it: ParameterValue) = {
     it match {
       case MapParameterValue(v) => new JObject(v.toList.map((f) => { (f._1.name, {
         f._2 match {
-          case LongPropertyValue(v) => JLong(v)
-          case StringPropertyValue(v) => JString(v)
-          case DoublePropertyValue(v) => JDouble(v)
-          case BooleanPropertyValue(v) => JBool(v)
+          case LongPropertyValue(v) => { JLong(v) }
+          case StringPropertyValue(v) => { JString(v) }
+          case DoublePropertyValue(v) => { JDouble(v) }
+          case BooleanPropertyValue(v) => { JBool(v) }
           case NullPropertyValue => JNull
-          case BooleanArrayPropertyValue(v) => {
-            println("@#@# why ??????")
-            null
-          }
-          case DoubleArrayPropertyValue(v) => {
-            println("@#@# why ??????")
-            null
-          }
-          case LongArrayPropertyValue(v) => {
-            println("@#@# why ??????")
-            null
-          }
-          case StringArrayPropertyValue(v) => {
-            println("@#@# why ??????")
-            null
-          }
-          case MapParameterValue(v) => {
-            println("@#@# why ??????")
-            null
-          }
-          case _ => {
-            println("@#@# why?")
-            null
-          }
         }
       })}))
-      case LongPropertyValue(v) => new JLong(v)
-      case StringPropertyValue(v) => new JString(v)
-      case DoublePropertyValue(v) => new JDouble(v)
-      case BooleanPropertyValue(v) => new JBool(v)
+      case LongPropertyValue(v) => { new JLong(v) }
+      case StringPropertyValue(v) => { new JString(v) }
+      case DoublePropertyValue(v) => { new JDouble(v) }
+      case BooleanPropertyValue(v) => { new JBool(v) }
       case NullPropertyValue => JNull
-      case BooleanArrayPropertyValue(v) => {
-        println("@#@# why ??????")
-        null
-      }
-      case DoubleArrayPropertyValue(v) => {
-        println("@#@# why ??????")
-        null
-      }
-      case LongArrayPropertyValue(v) => {
-        println("@#@# why ??????")
-        null
-      }
-      case StringArrayPropertyValue(v) => {
-        println("@#@# why ??????")
-        null
-      }
-      case MapParameterValue(v) => {
-        println("@#@# why ??????")
-        null
-      }
-      case _ => {
-        println("@#@# why?")
-        null
-      }
     }
   }
 }
 
 class ParameterMapSerializer() extends CustomSerializer[ParameterMap](format => ( {
-  case _ => { println("Attempt to do parameter map 1"); null }
+  case _ => { null }
 }, {
   case p: ParameterMap => { new JObject(p.toList.map( (f) => { (f._1.name, ParameterMapSerializer.parameterValueOf(f._2))})) }
 }))
-
-
-
